f7fa1da89fc40c953ed68b804b60d966b0c464b5
PhelanGeek <guopengios@163.com>
2016-11-11 15:31:57 +0800

    更新

--- Move/Move/Supporting Files/AppDelegate.m
+++ Move/Move/Supporting Files/AppDelegate.m
@@ -181,28 +181,28 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
-//- (void)application:(UIApplication *)application performActionForShortcutItem:
-//(UIApplicationShortcutItem *)shortcutItem completionHandler:(void (^)(BOOL))completionHandler {
-//    
-//    
-//    if ([shortcutItem.type isEqual:@"first"])
-//    {
-//        _rootTabBarController.selectedIndex = 0;
-//
-//    }
-//    else if([shortcutItem.type isEqual:@"second"])
-//    {
-//        _rootTabBarController.selectedIndex = 3;
-//        
-//    }else if([shortcutItem.type isEqual:@"third"]){
-//        _rootTabBarController.selectedIndex = 1;
-//    }else {
-//        UINavigationController *myNAV = [_rootTabBarController.viewControllers objectAtIndex:2];
-//        PLRunNowViewController *runNowVC = [[PLRunNowViewController alloc] init];
-//        runNowVC.hidesBottomBarWhenPushed = YES;
-//        [myNAV pushViewController:runNowVC animated:YES];
-//    }
-//}
+- (void)application:(UIApplication *)application performActionForShortcutItem:
+(UIApplicationShortcutItem *)shortcutItem completionHandler:(void (^)(BOOL))completionHandler {
+    
+    
+    if ([shortcutItem.type isEqual:@"first"])
+    {
+        _rootTabBarController.selectedIndex = 0;
+
+    }
+    else if([shortcutItem.type isEqual:@"second"])
+    {
+        _rootTabBarController.selectedIndex = 3;
+        
+    }else if([shortcutItem.type isEqual:@"third"]){
+        _rootTabBarController.selectedIndex = 1;
+    }else {
+        UINavigationController *myNAV = [_rootTabBarController.viewControllers objectAtIndex:2];
+        PLRunNowViewController *runNowVC = [[PLRunNowViewController alloc] init];
+        runNowVC.hidesBottomBarWhenPushed = YES;
+        [myNAV pushViewController:runNowVC animated:YES];
+    }
+}
 
 
 
